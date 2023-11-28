77b69f531c6f7a07b9a254f49f81ab0b84f5ca2c
elim316 <2200832@sit.singaporetech.edu.sg>
2023-11-28 17:57:27 +0800

    Add new binary files and progress marks

---
+++ build/robot_car/motor/CMakeFiles/motor.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for motor.
+# This may be replaced when dependencies are built.
---
+++ build/robot_car/ultrasonic/CMakeFiles/pico_ultrasonic_object.dir/ultrasonicObject.cpp.obj.d
@@ -0,0 +1,4 @@
+robot_car/ultrasonic/CMakeFiles/pico_ultrasonic_object.dir/ultrasonicObject.cpp.obj: \
+ /Users/elias/Documents/GitHub/embeddedprj/robot_car/ultrasonic/ultrasonicObject.cpp \
+ /Users/elias/Documents/GitHub/embeddedprj/robot_car/ultrasonic/ultrasonicObject.h \
+ /Users/elias/Documents/GitHub/embeddedprj/robot_car/ultrasonic/ultrasonic.h
