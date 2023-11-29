c057c5d715ccd5f271d2ab1b639cf1f060b53859
MRizqullah <mrizqullahhafizh@gmail.com>
2023-11-29 18:05:39 +0800

    Refactor encoder files

--- driver/encoder/encoder.c
+++ driver/encoder/encoder.c
@@ -4,114 +4,123 @@
 
 #include "encoder.h"
 
+// Encoder pin definitions
 #define GPIO_PIN_ENC_LEFT 2
 #define GPIO_PIN_ENC_RIGHT 3
 
+// Define buffer size for storing encoder interrupt counts
 #define INTERRUPT_BUF_SIZE 5
 
+// Encoder disc parameters
 #define ENCODER_DISC_SLOTS 20
-#define WHEEL_DIAMETER_CM 6.7                             // Centimeter
-#define WHEEL_CIRCUMFERENCE_CM (M_PI * WHEEL_DIAMETER_CM) // Centimeter
+#define WHEEL_DIAMETER_CM 6.7 // Wheel diameter in centimeters
 
+// Calculate wheel circumference
+const float WHEEL_CIRCUMFERENCE_CM = M_PI * WHEEL_DIAMETER_CM;
+
+// Distance per encoder slot
 const float CM_PER_SLOT = WHEEL_CIRCUMFERENCE_CM / ENCODER_DISC_SLOTS;
 
-struct repeating_timer timer, isrAlertTimer;
+// Timer for wheel speed calculation
+struct repeating_timer timer;
 
-// Wheel speed calculation
-volatile int encIndex = 0;
-volatile int leftInterruptBuffer[INTERRUPT_BUF_SIZE];
-volatile int rightInterruptBuffer[INTERRUPT_BUF_SIZE];
+// Timer for precise distance movement and angle spot turns
+struct repeating_timer isrAlertTimer;
 
-// Encoder interrupt counting (used during precise distance movement and angle spot turns)
-volatile int enableISRCounter = 0;
-volatile int encoderISRLeftTargetReached = 0;
-volatile int encoderISRRightTargetReached = 0;
-volatile uint encoderISRTarget = 0;
-volatile uint encoderISRLeftCounter = 0;
-volatile uint encoderISRRightCounter = 0;
+// Wheel speed calculation variables
+volatile int encIndex = 0;                             // Index for interrupt buffer
+volatile int leftInterruptBuffer[INTERRUPT_BUF_SIZE];  // Interrupt buffer for left encoder
+volatile int rightInterruptBuffer[INTERRUPT_BUF_SIZE]; // Interrupt buffer for right encoder
 
+// Encoder interrupt counting variables
+volatile int enableISRCounter = 0;             // Flag to enable/disable ISR counter
+volatile int encoderISRLeftTargetReached = 0;  // Flag indicating left encoder has reached target count
+volatile int encoderISRRightTargetReached = 0; // Flag indicating right encoder has reached target count
+volatile uint encoderISRTarget = 0;            // Target interrupt count
+volatile uint encoderISRLeftCounter = 0;       // Current interrupt count for left encoder
+volatile uint encoderISRRightCounter = 0;      // Current interrupt count for right encoder
+
+// Encoder interrupt handler
 void ISR_encoder(uint gpio, uint32_t events)
 {
     if (gpio == GPIO_PIN_ENC_LEFT)
     {
-        leftInterruptBuffer[encIndex]++;
+        leftInterruptBuffer[encIndex]++; // Increment left encoder interrupt count
+
         if (enableISRCounter && !encoderISRLeftTargetReached)
-        {
-            encoderISRLeftCounter++;
+        {                            // Check if ISR counter is enabled and left encoder target not reached
+            encoderISRLeftCounter++; // Increment left encoder ISR counter
 
             if (encoderISRLeftCounter == encoderISRTarget)
-                encoderISRLeftTargetReached = 1;
+            {                                    // Check if left encoder ISR counter has reached target
+                encoderISRLeftTargetReached = 1; // Set flag indicating left encoder target reached
+            }
         }
-        // printf("L encoder interrupt \n");
     }
 
     if (gpio == GPIO_PIN_ENC_RIGHT)
     {
-        rightInterruptBuffer[encIndex]++;
+        rightInterruptBuffer[encIndex]++; // Increment right encoder interrupt count
+
         if (enableISRCounter && !encoderISRRightTargetReached)
-        {
-            encoderISRRightCounter++;
+        {                             // Check if ISR counter is enabled and right encoder target not reached
+            encoderISRRightCounter++; // Increment right encoder ISR counter
 
             if (encoderISRRightCounter == encoderISRTarget)
-                encoderISRRightTargetReached = 1;
+            {                                     // Check if right encoder ISR counter has reached target
+                encoderISRRightTargetReached = 1; // Set flag indicating right encoder target reached
+            }
         }
-        // printf("R encoder interrupt \n");
     }
 }
 
+// Timer callback for wheel speed calculation
 bool timerCallback(struct repeating_timer *timer)
 {
-    /*
-    ===== How the speed calculation works =====
-    1. Both encoder has its own int[4] buffer (+1 for the current buffer, which may not be complete result yet)
-    2. A timer is setup to trigger this callback every 0.25s
-    3. Stores the amount of interrupts since the last callback
-    4. The buffer is filled sequentially with index incremented each time, wrapping back to 0 at max index
-    5. The speed is calculated by summing up values in the buffer (average interrupts/second for the last second)
-    */
+    // Update index for interrupt buffer
     encIndex++;
     if (encIndex == INTERRUPT_BUF_SIZE)
+    {
         encIndex = 0;
+    }
+
+    // Reset interrupt buffers for new calculations
     leftInterruptBuffer[encIndex] = 0;
     rightInterruptBuffer[encIndex] = 0;
+
+    // Return true to continue timer
     return true;
 }
 
+// Initialize encoder hardware and timer for wheel speed calculation
 void ENCODER_init(void)
 {
     printf("[Encoder] Init start \n");
 
-    // Setup timer (0.25 second), used to calculate wheel speed
-    // Create a repeating timer that calls repeating_timer_callback.
-    // If the delay is > 0 then this is the delay between the previous callback ending and the next starting.
-    // If the delay is negative then the next call to the callback will be exactly delay ms after the start of the call to the last callback
+    // Set up timer to trigger wheel speed calculation callback every 0.25 seconds
     add_repeating_timer_ms(-250, &timerCallback, NULL, &timer);
 
-    // Setup interrupt, trigger during high to low transition
+    // Set up encoder interrupts to trigger on falling edge (high to low transition)
     gpio_set_irq_enabled_with_callback(GPIO_PIN_ENC_LEFT, GPIO_IRQ_EDGE_FALL, true, &ISR_encoder);
     gpio_set_irq_enabled_with_callback(GPIO_PIN_ENC_RIGHT, GPIO_IRQ_EDGE_FALL, true, &ISR_encoder);
 
     printf("[Encoder] Init done \n");
 }
 
-// Returns wheel speed in centimeter/sec
+// Get wheel speed in centimeters per second
 float ENCODER_getWheelSpeed(int encoder)
 {
-    int interruptsPerSec = ENCODER_getWheelInterruptSpeed(encoder);
-    return interruptsPerSec * CM_PER_SLOT;
-}
-
-// Returns wheel speed in interrupts/sec
-int ENCODER_getWheelInterruptSpeed(int encoder)
-{
-    int total = 0;
+    // Calculate total interrupts for specified encoder
+    int totalInterrupts = 0;
     if (encoder & ENCODER_LEFT)
     {
         for (int i = 0; i < INTERRUPT_BUF_SIZE; i++)
         {
             if (i == encIndex)
+            {
                 continue;
-            total += leftInterruptBuffer[i];
+            }
+            totalInterrupts += leftInterruptBuffer[i];
         }
     }
     else if (encoder & ENCODER_RIGHT)
@@ -119,58 +128,15 @@ int ENCODER_getWheelInterruptSpeed(int encoder)
         for (int i = 0; i < INTERRUPT_BUF_SIZE; i++)
         {
             if (i == encIndex)
+            {
                 continue;
-            total += rightInterruptBuffer[i];
+            }
+            totalInterrupts += rightInterruptBuffer[i];
         }
     }
-    return total;
-}
 
-void ENCODER_waitForISRInterrupts(uint target)
-{
-    // Reset and enable ISR counter
-    encoderISRLeftCounter = encoderISRRightCounter = 0;
-    encoderISRLeftTargetReached = encoderISRRightTargetReached = 0;
-    encoderISRTarget = target;
-    enableISRCounter = 1;
+    // Convert interrupts to wheel speed in centimeters per second
+    float wheelSpeed = totalInterrupts * CM_PER_SLOT;
 
-    // Block until interrupt count reaches target
-    while (!encoderISRLeftTargetReached || !encoderISRRightTargetReached)
-        sleep_ms(10);
-
-    enableISRCounter = 0;
-}
-
-bool timerCallbackISRAlert(struct repeating_timer *timer)
-{
-    if (enableISRCounter)
-    {
-        if (encoderISRLeftTargetReached && encoderISRRightTargetReached)
-        {
-            repeating_timer_callback_t callback = (repeating_timer_callback_t)(timer->user_data);
-            enableISRCounter = 0;
-            return callback(timer); // Stops the timer
-        }
-        else
-        {
-            return true;
-        }
-    }
-}
-
-void ENCODER_alertAfterISRInterrupts(uint target, repeating_timer_callback_t callback)
-{
-    // Reset and enable ISR counter
-    encoderISRLeftCounter = encoderISRRightCounter = 0;
-    encoderISRLeftTargetReached = encoderISRRightTargetReached = 0;
-    encoderISRTarget = target;
-    enableISRCounter = 1;
-
-    add_repeating_timer_ms(-10, timerCallbackISRAlert, callback, &isrAlertTimer);
-}
-
-// Convert from centimeters to steps
-int ENCODER_cmToSteps(float cm)
-{
-    return (int)(cm / CM_PER_SLOT); // Convert to an integer (note this is NOT rounded)
+    return wheelSpeed;
 }
--- driver/encoder/encoder.h
+++ driver/encoder/encoder.h
@@ -1,15 +1,26 @@
-#ifndef encoder_H_
-#define encoder_H_
+#ifndef ENCODER_H_
+#define ENCODER_H_
 
+// Encoder identifiers
 #define ENCODER_LEFT 1
 #define ENCODER_RIGHT 2
 
+// Initialize encoder pins and interrupts
 void ENCODER_init(void);
+
+// Get current wheel speed in revolutions per second (RPS)
 float ENCODER_getWheelSpeed(int encoder);
+
+// Get current wheel interrupt speed in interrupts per second (IPS)
 int ENCODER_getWheelInterruptSpeed(int encoder);
 
+// Block until a specified number of interrupt counts have been reached
 void ENCODER_waitForISRInterrupts(uint target);
+
+// Set a callback function to be called after a specified number of interrupt counts have been reached
 void ENCODER_alertAfterISRInterrupts(uint target, repeating_timer_callback_t callback);
+
+// Convert a distance in centimeters (cm) to encoder steps
 int ENCODER_cmToSteps(float cm);
 
 #endif
